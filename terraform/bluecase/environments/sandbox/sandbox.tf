5933c9be620234937cffa9b2bb98e497fdc88199
Brian Gannon <briancgannon@gmail.com>
2019-02-15 12:19:22 -0500

    add sg, elb, asg

--- terraform/bluecase/environments/sandbox/sandbox.tf
+++ terraform/bluecase/environments/sandbox/sandbox.tf
@@ -4,6 +4,9 @@ provider "aws" {
   region                  = "us-east-2"
 }
 
+data "aws_availability_zones" "all" {}
+
+
 variable "ec2_testbox_ami" {
   type = "string"
   default = "ami-0f65671a86f061fcd"
@@ -14,20 +17,72 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "testbox" {
-  ami = "${var.ec2_testbox_ami}"
+resource "aws_launch_configuration" "testbox-lc" {
+  image_id = "${var.ec2_testbox_ami}"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.testbox-sg.id}"]
-
+  security_groups = ["${aws_security_group.testbox-sg.id}"] 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags {
-    Name = "terraform-testbox"
-    Environment = "sandbox"
+resource "aws_autoscaling_group" "testbox-asg" {
+  launch_configuration = "${aws_launch_configuration.testbox-lc.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.testbox-elb.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+  tag {
+    key = "Name"
+    value = "testbox-asg"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "testbox-elb" {
+  name = "testbox-elb"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.testbox-elb-sg.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "testbox-elb-sg" {
+  name = "testbox-elb-sg"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -41,6 +96,6 @@ resource "aws_security_group" "testbox-sg" {
   }
 }
 
-output "public_ip" {
-  value = "${aws_instance.testbox.public_ip}"
+output "elb_dns_name" {
+  value = "${aws_elb.testbox-elb.dns_name}"
 }

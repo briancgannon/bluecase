e89680ced719e655d6fbce06f22e2f77ccbe7fbc
Brian Gannon <briancgannon@gmail.com>
2019-02-15 14:41:20 -0500

    all kinds of stuff

--- terraform/bluecase/ec2.tf
+++ terraform/bluecase/ec2.tf
@@ -1,5 +1,5 @@
 esource "aws_launch_configuration" "testbox-lc" {
-  image_id = "${var.ubuntu18_ami}"
+  image_id = "${var.basic_server_ami}"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.testbox-sg.id}"] 
   user_data = <<-EOF
@@ -14,7 +14,7 @@ esource "aws_launch_configuration" "testbox-lc" {
 
 resource "aws_autoscaling_group" "testbox-asg" {
   launch_configuration = "${aws_launch_configuration.testbox-lc.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = ["${var.availability_zones}"]
 
   load_balancers = ["${aws_elb.testbox-elb.name}"]
   health_check_type = "ELB"

b52175f754f289a2afcd6d05484b783f2cc464ac
Brian Gannon <briancgannon@gmail.com>
2019-02-18 15:00:56 -0500

    add backend state configs for prod

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-sandbox-"
-    key    = "sandbox/terraform.tfstate"
+    bucket = "terraform-state-production-"
+    key    = "production/terraform.tfstate"
     encrypt = true
     region  = "us-east-2"
-    dynamodb_table = "terraform-state-sandbox"
+    dynamodb_table = "terraform-state-production"
   }
 
   required_version = "=0.11.8"

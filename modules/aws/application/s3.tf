2269da7dcc27e0767abab7fd9d9db19ff66021c1
Brian Gannon <briancgannon@gmail.com>
2019-02-28 10:03:29 -0500

    whitespace

--- modules/aws/application/s3.tf
+++ modules/aws/application/s3.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket" "bluecase-bucket" {
    bucket_prefix = "bluecase-${var.env_name}-"
    acl           = "private"
 
-
     tags {
       Name = "bluecase-${var.env_name}"
     }
